295a2f7b3be299bdd622ef317048fb49097cca16
Jaswin Mulagapaka <mulagapakajaswin.261ec233@nitk.edu.in>
2026-09-13 12:02:36 +0530

    Time: 0 ms (100%), Space: 20.4 MB (9.15%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,37 +1,13 @@
-
-
-
-
-
-
-
-
-
-
-
-
 void moveZeroes(int* nums, int numsSize) {
     int k = 0;
-
-    // Move non-zero elements forward
     for (int i = 0; i < numsSize; i++) {
         if (nums[i] != 0) {
-            nums[k] = nums[i];
+            if (i != k) {
+                int temp = nums[k];
+                nums[k] = nums[i];
+                nums[i] = temp;
+            }
             k++;
         }
     }
-
-    // Fill the rest with zeros
-    for (int i = k; i < numsSize; i++) {
-        nums[i] = 0;
-    }
-
-
-
-
-    // Print result
-    for (int i = 0; i < numsSize; i++) {
-        printf("%d ", nums[i]);
-    }
- 
 }
