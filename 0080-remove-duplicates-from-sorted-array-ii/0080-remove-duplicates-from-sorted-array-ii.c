ee4d89a1b4edb294b5a70768ec6b49a17eee46fc
Jaswin Mulagapaka <mulagapakajaswin.261ec233@nitk.edu.in>
2026-09-14 15:47:12 +0530

    Time: 29 ms (54.37%), Space: 15.2 MB (56.34%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
@@ -3,11 +3,9 @@ int removeDuplicates(int* nums, int numsSize) {
         return numsSize;
     }
 
-    int k = 2; // nums[0] and nums[1] are always kept
-
+    int k = 2; 
     for (int i = 2; i < numsSize; i++) {
-        // Since array is sorted, nums[i] is valid as long as
-        // it differs from the element 2 positions back
+     
         if (nums[i] != nums[k - 2]) {
             nums[k] = nums[i];
             k++;
