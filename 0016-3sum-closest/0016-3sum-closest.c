9ca9c4380c925f388e4d5155e9cd811978482b5a
Jaswin Mulagapaka <mulagapakajaswin.261ec233@nitk.edu.in>
2026-09-17 21:13:38 +0530

    Time: 17 ms (28.97%), Space: 9 MB (84.53%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.c
+++ 0016-3sum-closest/0016-3sum-closest.c
@@ -1,18 +1,15 @@
 
 
 void bubbleSort(int* nums, int n) {
-    for (int i = 0; i < n ; i++) {
-        int swapped = 0;
-        for (int j = 0; j < n - i - 1; j++) {
-            if (nums[j] > nums[j + 1]) {
-                int temp = nums[j];
-                nums[j] = nums[j + 1];
-                nums[j + 1] = temp;
-                swapped = 1;
-            }
+   for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+        if (nums[j] > nums[j + 1]) {
+            int temp = nums[j];
+            nums[j] = nums[j + 1];
+            nums[j + 1] = temp;
         }
-        if (!swapped) break;
     }
+}
 }
 
 int threeSumClosest(int* nums, int numsSize, int target) {
